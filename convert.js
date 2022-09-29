54121c1ef7b4b694e2574dc6be11eb1f4f2817e8
hothanhptit <hothanhptit@gmail.com>
2022-09-30 00:12:40 +0700

    cant change freq withoout bitrate

--- convert.js
+++ convert.js
@@ -2,6 +2,16 @@ var ffmpeg = require("fluent-ffmpeg");
 const ffmpegPath = require("@ffmpeg-installer/ffmpeg").path;
 // const ffprobePath = require('@ffprobe-installer/ffprobe').path;
 
+// ffmpeg()
+//   .addInput("./1.mp3")
+//   .inputFormat("mp3")
+//   .audioCodec("pcm_u8") //8bit unsigned
+//   // .audioCodec("pcm_s16le")
+//   .format("wav")
+//   .setStartTime(0)
+//   .setDuration(30000)
+  // .save(`/cut.mp3`);
+
 const convert = (output) => {
   //   let wav = ffmpeg()
   //     .addInput("/2.mp3")
@@ -10,18 +20,25 @@ const convert = (output) => {
   //     .save(`${output}.wav`);
   //   let wav = ffmpeg().addInput("/1.mp3").audioBitrate(64);
   ffmpeg.setFfmpegPath(ffmpegPath);
-  console.log('Converting file...');
+  console.log("Converting file...");
   ffmpeg()
     .addInput("./1.mp3")
     .inputFormat("mp3")
-    // .audioCodec("pcm_u8") //8bit unsigned
-    .audioCodec("pcm_s16le")
+    .audioCodec("pcm_u8") //8bit unsigned
+    .setStartTime(0)
+    .setDuration(45)
+    // .audioCodec("pcm_s16le")
     .format("wav")
     .audioChannels("1")
-    // .audioBitrate(128)
-    .audioFrequency(5678)
+    .audioFrequency(8000)
+
+    // .audioQuality(1024)
+    // .setStartTime("0")
+    // .setDuration("30000")
+    // .run()
+    // .setDuration(30000)
+    // .audioBitrate(64)
     // .audioFrequency(8000)
-    
     // .native()
     .save(`./${output}.wav`);
   //   console.log(wav);
